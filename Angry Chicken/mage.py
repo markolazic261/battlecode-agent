57b1c44a3ebf59dfd0aff49bb7ce735dac0881d0
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-04-18 12:20:31 +0200

    Fixed bug in mage

--- Angry Chicken/mage.py
+++ Angry Chicken/mage.py
@@ -83,7 +83,7 @@ class Mage(units.Unit):
                     if self.__outer._gc.has_unit_at_location(adjacent_location) and not my_units_map[adjacent_location.x][adjacent_location.y]:
                         current_target_nr_enemies += 1
                         current_target_hp += self.__outer._gc.sense_unit_at_location(adjacent_location).health
-                    elif my_units_map[adjacent_location.x][adjacent_location.y]:
+                    elif self.__outer._gc.has_unit_at_location(adjacent_location) and my_units_map[adjacent_location.x][adjacent_location.y]:
                         current_target_nr_enemies -= 1
                         current_target_hp += 2*self.__outer._gc.sense_unit_at_location(adjacent_location).health
 
